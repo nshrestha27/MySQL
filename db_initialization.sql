63d8764a597887cbbe164df3075d0c070ca5ce95
Nilam Shrestha <sthaneelu01@gmail.com>
2022-12-25 12:40:46 -0700

    Create a new database

---
+++ db_initialization.sql
@@ -0,0 +1,12 @@
+-- See what databases are available
+show databases;
+
+-- create a new database named society
+create database society;
+
+-- use database to view the tables
+use society;
+show tables;
+
+
+
